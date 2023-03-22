3e28e97b55c8af106f3f81469eee1c7f26e5be1b
RickyTheJenius <yukh31@gmail.com>
2023-03-22 12:59:16 -0700

    css done

---
+++ src/components/containerCompo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <postCompo/>
+    <postCompo/>
+    <postCompo/>
+  </div>
+</template>
+
+<script>
+import postCompo from './postCompo.vue'
+export default {
+    name:'containerCompo',
+    components:{
+        postCompo
+    }
+}
+</script>
+
+<style>
+    div{
+        background-color: bisque;
+    }
+</style>
